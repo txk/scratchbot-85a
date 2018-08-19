1c8c60d60580d8cfdd69593f8bb9893be396204a
koenig_t <thomas.x.koenig@questdiagnostics.com>
2018-08-19 15:37:03 -0400

    dev update

--- app.js
+++ app.js
@@ -39,5 +39,138 @@ var bot = new builder.UniversalBot(connector);
 bot.set('storage', inMemoryStorage);
 
 bot.dialog('/', function (session) {
-    session.send('You said LOCAL 3: ' + session.message.text);
+    //session.send('You said LOCAL 3: ' + session.message.text);
+
+    // Display Welcome card with Hotels and Flights search options
+    var card = {
+        'contentType': 'application/vnd.microsoft.card.adaptive',
+        'content': {
+            '$schema': 'http://adaptivecards.io/schemas/adaptive-card.json',
+            'type': 'AdaptiveCard',
+            'version': '1.0',
+            'body': [
+                {
+                    'type': 'Container',
+                    'speak': '<s>Hello!</s><s>Are you looking for a flight or a hotel?</s>',
+                    'items': [
+                        {
+                            'type': 'ColumnSet',
+                            'columns': [
+                                {
+                                    'type': 'Column',
+                                    'size': 'auto',
+                                    'items': [
+                                        {
+                                            'type': 'Image',
+                                            'url': 'https://placeholdit.imgix.net/~text?txtsize=65&txt=Adaptive+Cards&w=300&h=300',
+                                            'size': 'medium',
+                                            'style': 'person'
+                                        }
+                                    ]
+                                },
+                                {
+                                    'type': 'Column',
+                                    'size': 'stretch',
+                                    'items': [
+                                        {
+                                            'type': 'TextBlock',
+                                            'text': 'Hello!',
+                                            'weight': 'bolder',
+                                            'isSubtle': true
+                                        },
+                                        {
+                                            'type': 'TextBlock',
+                                            'text': 'Are you looking for a flight or a hotel?',
+                                            'wrap': true
+                                        }
+                                    ]
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ],
+            'actions': [
+                // Hotels Search form
+                {
+                    'type': 'Action.ShowCard',
+                    'title': 'Hotels',
+                    'speak': '<s>Hotels</s>',
+                    'card': {
+                        'type': 'AdaptiveCard',
+                        'body': [
+                            {
+                                'type': 'TextBlock',
+                                'text': 'Welcome to the Hotels finder!',
+                                'speak': '<s>Welcome to the Hotels finder!</s>',
+                                'weight': 'bolder',
+                                'size': 'large'
+                            },
+                            {
+                                'type': 'TextBlock',
+                                'text': 'Please enter your destination:'
+                            },
+                            {
+                                'type': 'Input.Text',
+                                'id': 'destination',
+                                'speak': '<s>Please enter your destination</s>',
+                                'placeholder': 'Miami, Florida',
+                                'style': 'text'
+                            },
+                            {
+                                'type': 'TextBlock',
+                                'text': 'When do you want to check in?'
+                            },
+                            {
+                                'type': 'Input.Date',
+                                'id': 'checkin',
+                                'speak': '<s>When do you want to check in?</s>'
+                            },
+                            {
+                                'type': 'TextBlock',
+                                'text': 'How many nights do you want to stay?'
+                            },
+                            {
+                                'type': 'Input.Number',
+                                'id': 'nights',
+                                'min': 1,
+                                'max': 60,
+                                'speak': '<s>How many nights do you want to stay?</s>'
+                            }
+                        ],
+                        'actions': [
+                            {
+                                'type': 'Action.Submit',
+                                'title': 'Search',
+                                'speak': '<s>Search</s>',
+                                'data': {
+                                    'type': 'hotelSearch'
+                                }
+                            }
+                        ]
+                    }
+                },
+                {
+                    'type': 'Action.ShowCard',
+                    'title': 'Flights',
+                    'speak': '<s>Flights</s>',
+                    'card': {
+                        'type': 'AdaptiveCard',
+                        'body': [
+                            {
+                                'type': 'TextBlock',
+                                'text': 'Flights is not implemented =(',
+                                'speak': '<s>Flights is not implemented</s>',
+                                'weight': 'bolder'
+                            }
+                        ]
+                    }
+                }
+            ]
+        }
+    };
+
+    var msg = new builder.Message(session)
+        .addAttachment(card);
+    session.send(msg);
 });
